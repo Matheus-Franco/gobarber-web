55557f2d8cbd14f2b76c888f0a55c601504810d1
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-07-31 14:52:19 -0300

    Add calendar to show the month availability

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,22 @@
-import React, { useRef, useCallback, useContext } from 'react';
-import { Form } from '@unform/web'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
-import { FormHandles } from '@unform/core'
-import * as Yup from 'yup'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useRef, useCallback } from 'react';
+import { Form } from '@unform/web';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 
-import { Container, Content, Background, AnimationContainer } from './styles'
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
 interface SignInFormData {
   email: string;
@@ -24,36 +24,36 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
-  const formRef = useRef<FormHandles>(null)
+  const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useAuth()
-  const { addToast } = useToast()
-  const history = useHistory()
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
-      formRef.current?.setErrors({})
+      formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
         email: Yup.string().required("E-mail obrigatório.").email("Digite um E-mail válido."),
         password: Yup.string().required("Senha obrigatória.")
-      })
+      });
 
       await schema.validate(data, {
         abortEarly: false
-      })
+      });
 
       await signIn({
         email: data.email,
         password: data.password
-      })
+      });
 
-      history.push('/dashboard')
+      history.push('/dashboard');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err)
+        const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors)
+        formRef.current?.setErrors(errors);
 
         return;
       }
@@ -61,13 +61,11 @@ const SignIn: React.FC = () => {
         type: "error",
         title: "Erro na autenticação",
         description: "Ococrreu um erro ao fazer login, verifique suas credenciais."
-      })
+      });
     }
   },
     [signIn, addToast, history]
-  )
-
-
+  );
 
   return (
     <Container >
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,22 +1,22 @@
 import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core'
-import { FiLogIn, FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi'
+import { FormHandles } from '@unform/core';
+import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { Link, useHistory } from 'react-router-dom';
 
-import api from '../../services/api'
+import api from '../../services/api';
 
-import { useToast } from '../../hooks/toast'
+import { useToast } from '../../hooks/toast';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import { Container, Content, Background, AnimationContainer } from './styles'
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
 interface SignUpFormData {
   name: string;
@@ -25,39 +25,39 @@ interface SignUpFormData {
 }
 
 const SignUp: React.FC = () => {
-  const formRef = useRef<FormHandles>(null)
-  const { addToast } = useToast()
-  const history = useHistory()
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: SignUpFormData) => {
     try {
-      formRef.current?.setErrors({})
+      formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
         name: Yup.string().required("Nome obrigatório."),
         email: Yup.string().required("E-mail obrigatório.").email("Digite um E-mail válido."),
         password: Yup.string().min(6, "No mínimo 6 digitos."),
-      })
+      });
 
       await schema.validate(data, {
         abortEarly: false
-      })
+      });
 
-      await api.post("/users", data)
+      await api.post("/users", data);
 
-      history.push('/')
+      history.push('/');
 
       addToast({
         type: "sucess",
         title: "Cadastro realizado.",
         description: "Você já pode fazer login"
-      })
+      });
 
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err)
+        const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors)
+        formRef.current?.setErrors(errors);
 
         return;
       }
@@ -65,9 +65,9 @@ const SignUp: React.FC = () => {
         type: "error",
         title: "Erro ao realizar cadastro.",
         description: "Ocorreu um erro ao fazer cadastro, tente novamente."
-      })
+      });
     }
-  }, [addToast, history])
+  }, [addToast, history]);
 
   return (
     <Container>
@@ -85,17 +85,14 @@ const SignUp: React.FC = () => {
             <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
             <Button type="submit">Cadastrar</Button>
-
           </Form>
 
           <Link to="/">
             <FiArrowLeft />
-        Voltar para Logon
-      </Link>
+             Voltar para Logon
+          </Link>
         </AnimationContainer>
       </Content>
-
-
     </Container>
   )
 }
